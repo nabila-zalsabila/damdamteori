7e7f88665f9da25d8119578099e7c9e95497b7ca
nabila-zalsabila <60900123017@uin-alauddin.ac.id>
2025-12-14 19:17:22 +0800

    code baru

--- klasifikasi.py
+++ klasifikasi.py
@@ -49,13 +49,22 @@ st.success(f"Kolom target terdeteksi: {target_col}")
 X = df.drop(target_col, axis=1)
 y = df[target_col]
 
+# =========================
+# SCALING DATA
+# =========================
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
 
 # =========================
-# TRAIN MODEL
+# SPLIT DATA
 # =========================
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
+
+# =========================
+# TRAIN MODEL
+# =========================
 
 model = LogisticRegression(max_iter=1000)
 model.fit(X_train, y_train)
